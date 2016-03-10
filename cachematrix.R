cea347cafddba5018de4450cddbb730c118c8750
helenwang94 <546338116@qq.com>
2016-03-10 16:16:48 +0800

    cachematrix.R
    Programming Assignment of Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The assignment is regarding lexical scoping and 
+## caching functions that may require a long computation time. 
 
-## Write a short comment describing this function
+## caculation
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function (y) {
+  x << -y
+  m << -NULL
 }
-
-
-## Write a short comment describing this function
+get <- fuction() x
+setmatrix <- function(solve) m<<- solve
+getmatirx <- function() m
+list(set=set, get=get,
+   setmatrix = setmatrix,
+   getmatrix = getmatrix)
+}
+## cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<-x$getmatrix()
+        if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+}
+matrix <- x$get()
+m <- solve (matrix,...)
+x$setmatrix(m)
+m
 }
