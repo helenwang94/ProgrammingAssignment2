d4d1c5dab69fc76ef3ec7f2a62799d5d5e705ab1
helenwang94 <546338116@qq.com>
2016-03-11 17:35:34 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,41 @@
 ## The assignment is regarding lexical scoping and 
 ## caching functions that may require a long computation time. 
 
-## caculation
+## makeCacheMatrix function is a list of functions which set and get the matrix value.
+## Also, they set the inversion of the matrix and get the inversion data.
 
-makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function (y) {
-  x << -y
-  m << -NULL
+makeCacheMatrix <- function(x = matrix()) 
+{
+  inv <- NULL
+  set <- function(y) 
+  {
+    x<<-y
+    inv<<-NULL
+  }
+  get <- function() x
+  setinv <- function(inversion) inv <<- inversion
+  getinv <- function() inv
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
-get <- fuction() x
-setmatrix <- function(solve) m<<- solve
-getmatirx <- function() m
-list(set=set, get=get,
-   setmatrix = setmatrix,
-   getmatrix = getmatrix)
-}
-## cache
 
-cacheSolve <- function(x, ...) {
+
+## cacheSolve function caches the inversion of matrix value in makeCacheMatrix function. 
+## Before caching, it checks if the data is already cached in the makeCacheMatrix function.
+## If it has done so, it will retrieve the same data
+
+
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
-        m<-x$getmatrix()
-        if(!is.null(m)) {
-         message("getting cached data")
-         return(m)
-}
-matrix <- x$get()
-m <- solve (matrix,...)
-x$setmatrix(m)
-m
+  inv <- x$getinv()
+  if(!is.null(inv)) 
+  {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
